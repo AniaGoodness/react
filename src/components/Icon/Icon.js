970e7c39bbb14424f03260c52b4dbaf7c8ed6018
Anna Grabowska <aniagoodness@gmail.com>
2020-08-09 00:26:48 +0200

    Add ESLint and husky

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// eslint-disable-next-line react/prop-types
 const Icon = props => (<i className={`fas fa-${props.name}`}></i>);
 
 Icon.protoTypes = {
-    name: PropTypes.node.isRequired,
-}
+  name: PropTypes.node.isRequired,
+};
 export default Icon;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -9,54 +9,54 @@ import ReactHtmlParser from 'react-html-parser';
 
 class List extends React.Component {
     state = {
-        columns: this.props.columns || [],
+      columns: this.props.columns || [],
     }
     static propTypes = { 
-        title: PropTypes.node.isRequired,
-        image: PropTypes.node,
-        addColumn: PropTypes.func,
-        description: PropTypes.node,
-        columns: PropTypes.array,
+      title: PropTypes.node.isRequired,
+      image: PropTypes.node,
+      addColumn: PropTypes.func,
+      description: PropTypes.node,
+      columns: PropTypes.array,
         
     }
 
     static defaultProps = { 
-        description: settings.defaultListDescription,
+      description: settings.defaultListDescription,
     }
 
     addColumn(title) {
-        this.setState(state => (
+      this.setState(state => (
+        {
+          columns: [
+            ...state.columns,
             {
-                columns: [
-                    ...state.columns,
-                    {
-                        key: state.columns.length ? state.columns[state.columns.length - 1].key + 1 : 0,
-                        title,
-                        icon: 'list-alt',
-                        cards: []
-                    }
-                ]
-            }
-        ));
+              key: state.columns.length ? state.columns[state.columns.length - 1].key + 1 : 0,
+              title,
+              icon: 'list-alt',
+              cards: [],
+            },
+          ],
+        }
+      ));
     }
     render() { 
-        return ( 
-            <section id="list" className={styles.component}>
-                <Hero titleText={this.props.title}
-                    imageFile={"http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"} />
-                <div className={styles.description}>
-                    {ReactHtmlParser(this.props.description)}
-                </div>
-                <div className={styles.columns}>
-                    {this.state.columns.map(({key, ...columnProps}) => ( 
-                        <Column key={key} {...columnProps} />
-                    ))} 
-                </div>
-                <div className={styles.creator}>
-                    <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)} />
-                </div>
-            </section>
-        );
+      return ( 
+        <section id="list" className={styles.component}>
+          <Hero titleText={this.props.title}
+            imageFile={'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'} />
+          <div className={styles.description}>
+            {ReactHtmlParser(this.props.description)}
+          </div>
+          <div className={styles.columns}>
+            {this.state.columns.map(({key, ...columnProps}) => ( 
+              <Column key={key} {...columnProps} />
+            ))} 
+          </div>
+          <div className={styles.creator}>
+            <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)} />
+          </div>
+        </section>
+      );
     }
 }
 
