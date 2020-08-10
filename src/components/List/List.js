7ecec0377f5afc21e9c04378c46ff449c51dc0eb
Anna Grabowska <aniagoodness@gmail.com>
2020-08-10 14:49:22 +0200

    redux1

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,59 +5,59 @@ import Column from '../Column/Column.js';
 import Creator from '../Creator/Creator.js';
 import PropTypes from 'prop-types';
 import { settings } from '../../data/dataStore';
-import ReactHtmlParser from 'react-html-parser'; 
+import ReactHtmlParser from 'react-html-parser';
 
 class List extends React.Component {
-    state = {
-      columns: this.props.columns || [],
-    }
-    static propTypes = { 
-      title: PropTypes.node.isRequired,
-      image: PropTypes.node,
-      addColumn: PropTypes.func,
-      description: PropTypes.node,
-      columns: PropTypes.array,
-        
-    }
+  state = {
+    columns: this.props.columns || [],
+  }
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    image: PropTypes.node,
+    addColumn: PropTypes.func,
+    description: PropTypes.node,
+    columns: PropTypes.array,
 
-    static defaultProps = { 
-      description: settings.defaultListDescription,
-    }
+  }
 
-    addColumn(title) {
-      this.setState(state => (
-        {
-          columns: [
-            ...state.columns,
-            {
-              key: state.columns.length ? state.columns[state.columns.length - 1].key + 1 : 0,
-              title,
-              icon: 'list-alt',
-              cards: [],
-            },
-          ],
-        }
-      ));
-    }
-    render() { 
-      return ( 
-        <section id="list" className={styles.component}>
-          <Hero titleText={this.props.title}
-            imageFile={'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'} />
-          <div className={styles.description}>
-            {ReactHtmlParser(this.props.description)}
-          </div>
-          <div className={styles.columns}>
-            {this.state.columns.map(({key, ...columnProps}) => ( 
-              <Column key={key} {...columnProps} />
-            ))} 
-          </div>
-          <div className={styles.creator}>
-            <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)} />
-          </div>
-        </section>
-      );
-    }
+  static defaultProps = {
+    description: settings.defaultListDescription,
+  }
+
+  addColumn(title) {
+    this.setState(state => (
+      {
+        columns: [
+          ...state.columns,
+          {
+            key: state.columns.length ? state.columns[state.columns.length - 1].key + 1 : 0,
+            title,
+            icon: 'list-alt',
+            cards: [],
+          },
+        ],
+      }
+    ));
+  }
+  render() {
+    return (
+      <section id="list" className={styles.component}>
+        <Hero titleText={this.props.title}
+          imageFile={'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'} />
+        <div className={styles.description}>
+          {ReactHtmlParser(this.props.description)}
+        </div>
+        <div className={styles.columns}>
+          {this.state.columns.map(({ key, ...columnProps }) => (
+            <Column key={key} {...columnProps} />
+          ))}
+        </div>
+        <div className={styles.creator}>
+          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)} />
+        </div>
+      </section>
+    );
+  }
 }
 
 export default List;
