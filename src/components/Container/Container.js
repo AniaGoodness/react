ddd29d3e0cb1934c29eb06895405107bd862cf55
Anna Grabowska <aniagoodness@gmail.com>
2020-08-24 19:38:34 +0200

    addContainer

---
+++ src/components/Container/Container.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Container';
+import PropTypes from 'prop-types';
+
+const Container = () => (
+  <div className={styles.component}>
+  </div>
+);
+Container.propTypes = {
+
+};
+export default Container;
